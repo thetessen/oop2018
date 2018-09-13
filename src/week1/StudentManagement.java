e3effd2e90faf1ef71360b72f0bf55881b7397ed
Thế Tessen <thesondong@gmail.com>
2018-09-13 22:40:11 +0700

    update

--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -3,7 +3,7 @@ package week1;
 public class StudentManagement {
 
     // TODO khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
-
+	Student[] sv = new Student[100];
     public static boolean sameGroup(Student s1, Student s2) {
         // TODO:
 		if(s1.getGroup()== s2.getGroup()) return true;		
