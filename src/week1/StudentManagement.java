1fd128945058caf80623bd6a6e6a1c1610343152
Thế Tessen <thesondong@gmail.com>
2018-09-17 17:37:04 +0700

    hoàn chỉnh

--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 public class StudentManagement {
 
     // TODO khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
-	Student[] sv = new Student[100];
+	public   ArrayList<Student> sv = new ArrayList<Student>(100);
     public static boolean sameGroup(Student s1, Student s2) {
         // TODO:
 		if(s1.getGroup().equals(s2.getGroup())) return true;	
