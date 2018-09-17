eadf1600d142ed798b7bb66bccb28c691f6762db
thetessen <43162598+thetessen@users.noreply.github.com>
2018-09-17 23:48:01 +0700

    hoan chinh

--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -26,7 +26,18 @@ public class StudentManagement {
 
     void removeStudent(String id) {
         // TODO   
-    	  
+    	int count=0, index=0;
+        while(sv[++count] != null);	
+        for(int i=0 ; i<count ; i++) {
+        	if(sv[i].getSid().equals(id)) {
+        		index=i;
+        		break;
+        	}
+        }
+        for(int i=index ; i<count-1 ; i++) {
+    		sv[i]=sv[i+1];
+    	}
+        sv[count-1]=null;  
    }
 
     public static void main(String[] args) {
@@ -34,19 +45,26 @@ public class StudentManagement {
     	nvThe.setGroup("INT22045");
     	System.out.println("Ten sinh vien: " + nvThe.getName());
     	nvThe.getInfo();
+	    
     	Student s1 = new Student("The","17021234","xxx.@gmail.com");
     	Student s2 = new Student();
     	Student s3 = new Student(s1);
-    	s3.setGroup("INT22042");  	
+    	s3.setGroup("INT22042");
+	    
     	StudentManagement sinhvien = new StudentManagement();
     	sinhvien.sv[0]=new Student(s1);
     	sinhvien.sv[1]=new Student(s2);
     	sinhvien.sv[2]=new Student(s3);
+	    
     	if(sinhvien.sameGroup(sinhvien.sv[0] ,sinhvien.sv[1])) {
    		 System.out.println("co cung lop");
     	} 
     	else System.out.println("khong cung lop");
     	sinhvien.studentsByGroup();
+	    
  //   	sinhvien.removeStudent("000");
+//    	for(int i=0;sinhvien.sv[i]!=null;i++) {
+//    		System.out.println(sinhvien.sv[i].getName());
+//    	}
     }
 }
