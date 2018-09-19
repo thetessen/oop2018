df5879c78f3c7e4024a72e8f2786b01ae750dac4
Thế Tessen <thesondong@gmail.com>
2018-09-19 18:22:27 +0700

    task1

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -4,11 +4,17 @@ public class Task1 {
 
     public static int gcd(int a, int b) {
         // TODO: Tính ước chung lớn nhất của 2 số a, b
-        return 0;
+		while(a*b !=0){
+			if(a>b)	a%=b;
+			else b%=a;
+		}
+        return a+b;
     }
 
     public static int fibonacci(int n) {
         // TODO: Tìm số fibonacci ở vị trí n
-        return 0;
+		if(n==0) return 0;
+			else if(n==1) return 1;
+        else return fibonacci(n-1)+fibonacci(n-2);
     }
 }
