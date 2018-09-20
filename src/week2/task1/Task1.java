851de0432db232fa3fcad5a08f038e93a57a743e
Thế Tessen <thesondong@gmail.com>
2018-09-20 21:22:56 +0700

    fibonacci task 1

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -17,7 +17,17 @@ public class Task1 {
         // TODO: Tìm số fibonacci ở vị trí n
 		if(n==0) return 0;
 			else if(n==1) return 1;
-        else return fibonacci(n-1)+fibonacci(n-2);
+        else {
+        	int a = 0;
+        	int b = 1;
+        	int c = 0;
+        	for(int i=2 ; i<=n ; i++) {
+        		c = a+b;
+        		a = b;
+        		b = c;
+        	}
+        	return c;
+        }
     }
 	public static void main(String[] args) {
     	System.out.println(gcd(4,15));
