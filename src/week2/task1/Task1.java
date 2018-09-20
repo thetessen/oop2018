890b66d12e3dffb1cb8d3027ee25e1dc7c1d1f60
Thế Tessen <thesondong@gmail.com>
2018-09-20 09:13:33 +0700

    update task 1

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -4,6 +4,8 @@ public class Task1 {
 
     public static int gcd(int a, int b) {
         // TODO: Tính ước chung lớn nhất của 2 số a, b
+		a=Math.abs(a);
+		b=Math.abs(b);
 		while(a*b !=0){
 			if(a>b)	a%=b;
 			else b%=a;
